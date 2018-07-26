a127a88f7c0f42d1a4ea95b03e5e9d6a198e8e24
eslawek <ellieslawek@icloud.com>
2018-07-26 18:24:51 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -1,13 +1,37 @@
+MUSIC = {
+  :Taylor_Swift => ["You Belong with Me", "Burn", "Our Song"],
+  :Imagine_Dragons => ["Radioactive", "Demons", "It's Time"],
+  :One_Direction => ["What Makes You Beautiful", "Magic", "I Would"]
+}
+
 
 #1. Define a method named `list_artists`. This method should use the `each_key` method to iterate over the hash and return all the artists (the keys)
 # This method should accept a hash as an argument.
 
+# def list_artists(artists)
+# puts (artists).keys
+# end
+# list_artists(MUSIC)
+
 #2. Define a method named `list_songs`. This method should use the `each_value` method to iterate over the music library hash and print out the name of the songs in the library.
 # This method should accept a hash as an argument.
+
+def list_songs(songs)
+ puts (songs).values
+end
+list_songs(MUSIC)
+
 
 #3. Define a method `delete_artist`. This method should take delete the key-value pair from the hash based on the artist and returned the updated hash.
 #This method should take two arguments, the music library hash, and the artist. Remember, keys in hashes are symbols, so you'll need to expect the method to accept the artist as a symbol. 
 
+def delete_artist(artist)
+  new_hash = MUSIC.delete(artist)
+  puts new_hash
+
+end
+
+delete_artist(Taylor_Swift)
 
 #4. This method should add a new artist and song (key value pair) to the hash and return the updated hash. 
 # This method should accept three arguments, the library hash, the artist, and the song.
